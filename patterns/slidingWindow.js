d4b508f53500cee1bed2982e0363d5da5b5d5c59
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-09-20 18:23:46 +0600

    sliding window pattern implemented for largetstsub array sum

--- patterns/slidingWindow.js
+++ patterns/slidingWindow.js
@@ -1,1 +1,15 @@
 "use strict";
+const maxSubArraySum = (arr, elements) => {
+    let maxSum = 0;
+    let tempSum = 0;
+    for (let i = 0; i < elements; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = elements; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - elements] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+};
+console.log(maxSubArraySum([10, 2, 5, 7, 2, 5], 3));
---
+++ patterns/slidingWindow.ts
@@ -0,0 +1,15 @@
+const maxSubArraySum = (arr: number[], elements: number) => {
+  let maxSum = 0;
+  let tempSum = 0;
+  for (let i = 0; i < elements; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = elements; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - elements] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+};
+
+console.log(maxSubArraySum([10, 2, 5, 7, 2, 5], 3));
