e8ab9cdb0e920fba73bdb5c695f2cb6680100a5a
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-09-20 09:19:56 +0600

    tested frequency counter

--- patterns/frequencyCounter.ts
+++ patterns/frequencyCounter.ts
@@ -1,4 +1,20 @@
-console.log("data");
-
-let data: string = "the big man";
-console.log(data);
+const same = (arr1: number[], arr2: number[]) => {
+  if (arr1.length !== arr2.length) return false;
+  const obj1: any = {};
+  const obj2: any = {};
+  for (let i of arr1) {
+    obj1[i] = (obj1[i] || 0) + 1;
+  }
+  for (let i of arr2) {
+    obj2[i] = (obj2[i] || 0) + 1;
+  }
+  for (const key in obj1) {
+    // @ts-ignore
+    if (!(key ** 2 in obj2)) {
+      return false;
+    }
+    if (obj2[key ** 2] !== obj1[key]) return false;
+  }
+  return true;
+};
+same([1, 2, 3], [1, 4, 9]);
