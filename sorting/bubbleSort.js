00fb522805bcdd3eee09e7a2e66e8d1d8c127591
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-09-26 20:58:10 +0600

    optimized bubble sort

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,14 +1,19 @@
 "use strict";
 const bubbleSort = (arr) => {
+    let noSwap;
     for (let i = arr.length; i > 0; i--) {
+        noSwap = true;
         for (let j = 0; j < i - 1; j++) {
+            console.log(arr, arr[j], arr[j + 1]);
             if (arr[j] > arr[j + 1]) {
                 var temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
+                noSwap = false;
             }
         }
+        // if (noSwap) break;
     }
     return arr;
 };
-console.log(bubbleSort([34, 7, 23, 32, 5, 62, 32, 4, 12, 9]));
+console.log(bubbleSort([7, 1, 2, 3, 4, 5, 6]));
