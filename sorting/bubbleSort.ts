8d84b691db1600c22a6b3c041646e613ee66f62c
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-09-26 20:02:42 +0600

    created bubbleSort

---
+++ sorting/bubbleSort.ts
@@ -0,0 +1,14 @@
+const bubbleSort = (arr: number[]) => {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSort([34, 7, 23, 32, 5, 62, 32, 4, 12, 9]));
