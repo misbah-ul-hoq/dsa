45d2ae51f8707b1e42ffb41d9276f3464465fb58
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-09-27 16:24:48 +0600

    created algorithm for merging two sorted arrays

---
+++ sorting/mergeSortedArrays.js
@@ -0,0 +1,25 @@
+"use strict";
+const mergerSortedArrays = (arr1, arr2) => {
+    const result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex <= arr1.length - 1 && rightIndex <= arr2.length - 1) {
+        if (arr1[leftIndex] < arr2[rightIndex]) {
+            result.push(arr1[leftIndex]);
+            leftIndex++;
+        }
+        else {
+            result.push(arr2[rightIndex]);
+            rightIndex++;
+        }
+    }
+    if (leftIndex < arr1.length) {
+        result.push(...arr1.slice(leftIndex));
+    }
+    else if (rightIndex < arr2.length) {
+        result.push(...arr2.slice(rightIndex));
+    }
+    return result;
+};
+//[1, 3, 4] [2, 5, 6]
+console.log(mergerSortedArrays([1, 2, 4, 5], [2, 4, 5, 10, 15, 18]));
