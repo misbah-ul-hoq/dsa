3e21498b37f98655d7dec8499d717907cf46f930
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-10-03 21:04:52 +0600

    created unshift method for singly linked lists

--- data-structures/linked-lists/singlyLinkedLists.ts
+++ data-structures/linked-lists/singlyLinkedLists.ts
@@ -28,13 +28,51 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (this.length === 0) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift() {
+    if (this.length === 0) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  unshift(val: any) {
+    let newNode = new Nodes(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 const list = new SinglyLinkedList();
 
 list.push("some value");
-// console.log(list.push("some value two"));
+list.push("some value two");
 list.push("some value three");
-list.push("some value four");
-
-console.log(list.head.next);
+console.log(list.unshift("should be at the beginning"));
+console.log(list);
