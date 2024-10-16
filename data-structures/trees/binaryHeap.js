4d5a2d1cf8e56ee0b8adbd47e57e99ebf8c48e09
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-10-16 08:30:44 +0600

    created extract method on binary heaps

--- data-structures/trees/binaryHeap.js
+++ data-structures/trees/binaryHeap.js
@@ -23,10 +23,58 @@ class MaxBinaryHeap {
             index = parentIndex;
         }
     }
+    extractMax() {
+        const max = this.values[0];
+        const end = this.values.pop();
+        if (this.values.length)
+            this.values[0] = end;
+        this.sinkDown();
+        return max;
+    }
+    sinkDown() {
+        let index = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null; // [22, 25, 23]
+            if (leftChildIndex < length) {
+                leftChild = this.values[leftChildIndex];
+                if (leftChild > element) {
+                    swap = leftChildIndex;
+                }
+            }
+            if (rightChildIndex < length) {
+                rightChild = this.values[rightChildIndex];
+                if ((!swap && rightChild > element) ||
+                    (swap && rightChild > leftChild)) {
+                    swap = rightChildIndex;
+                }
+            }
+            if (!swap)
+                break;
+            this.values[index] = this.values[swap];
+            this.values[swap] = element;
+            index = swap;
+        }
+    }
 }
 const heap = new MaxBinaryHeap();
 heap.insert(15);
 heap.insert(20);
 heap.insert(30);
+heap.insert(45);
 heap.insert(35);
+heap.insert(65);
+heap.insert(33);
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
 console.log(heap);
