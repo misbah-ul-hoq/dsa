9c1f6cab2cfe3ac0cccc696df2d55a1698241321
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-10-20 21:58:54 +0600

    Created dequeue method on priorityQueue

--- data-structures/queues/priorityQueue.js
+++ data-structures/queues/priorityQueue.js
@@ -21,17 +21,58 @@ class PriorityQueue {
         while (index > 0) {
             let parentIndex = Math.floor((index - 1) / 2);
             let parent = this.values[parentIndex];
-            if (element.priority <= parent.priority)
+            if (element.priority >= parent.priority)
                 break;
             this.values[parentIndex] = element;
             this.values[index] = parent;
             index = parentIndex;
         }
     }
+    dequeue() {
+        let min = this.values[0];
+        let end = this.values.pop();
+        if (this.values.length)
+            this.values[0] = end;
+        this.sinkDown();
+        return min;
+    }
+    sinkDown() {
+        let index = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while (true) {
+            let leftChildIndex = index * 2 + 1;
+            let rightChildIndex = index * 2 + 2;
+            let leftChild, rightChild, swap;
+            if (leftChildIndex < length) {
+                leftChild = this.values[leftChildIndex];
+                if (element.priority > leftChild.priority) {
+                    swap = leftChildIndex;
+                }
+            }
+            if (rightChildIndex < length) {
+                rightChild = this.values[rightChildIndex];
+                if ((!swap && rightChild.priority < element.priority) ||
+                    (swap && rightChild.priority < leftChild.priority)) {
+                    swap = rightChildIndex;
+                }
+            }
+            if (!swap)
+                break;
+            this.values[index] = this.values[swap];
+            this.values[swap] = element;
+            index = swap;
+        }
+    }
 }
 const priorityQueue = new PriorityQueue();
-priorityQueue.enqueue("Morium", 2);
-priorityQueue.enqueue("Fatema", 1);
-priorityQueue.enqueue("Tabassum", 4);
-priorityQueue.enqueue("Tripty", 3);
-console.log(priorityQueue);
+priorityQueue.enqueue("Second Priority", 2);
+priorityQueue.enqueue("First", 1);
+priorityQueue.enqueue("Fourth", 4);
+priorityQueue.enqueue("Third", 3);
+priorityQueue.enqueue("Fifth", 5);
+console.log(priorityQueue.dequeue());
+console.log(priorityQueue.dequeue());
+console.log(priorityQueue.dequeue());
+console.log(priorityQueue.dequeue());
+console.log(priorityQueue.dequeue());
